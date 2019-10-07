14725589f400c89b14bdfe460b67307920d6fe1e
ynzy <758922096@qq.com>
2019-10-07 21:22:09 +0800

    feat(module): koa路由参数获取

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const Koa = require('koa')
-// const Router = require('koa-router')
+const Router = require('koa-router')
+const parser = require('koa-bodyparser')
 
 const InitManager = require('./core/init')
 
@@ -7,10 +8,12 @@ const InitManager = require('./core/init')
 const app = new Koa()
 // const router = new Router()
 
+app.use(parser())
+
 InitManager.initCore(app)
 
 
-const port = 3000  //端口号
+const port = 9000  //端口号
 
 app.listen(port, () => {
   console.log(`程序启动,请访问http://localhost:${port}`);
--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,7 +1,12 @@
 const Router = require('koa-router')
 const router = new Router()
 
-router.get('/classic/latest', (ctx, next) => {
+router.post('/v1/classic/latest/:id', (ctx, next) => {
+  const path = ctx.params
+  const query = ctx.query
+  const headers = ctx.header
+  const body = ctx.request.body
+
   ctx.body = {
     key: 'classic'
   }
