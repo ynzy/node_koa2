3b7a8fc38f81b54256656f8eb25484f19536c8a7
qiu rui <857694115>
2019-11-01 22:13:45 +0800

    feat(module:all): 使用bcrypt加密密码

--- app/api/v1/user.js
+++ app/api/v1/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs')
 const Router = require('koa-router')
 const { RegisterValidator } = require('../../validators/validator')
 const { User } = require('../../models/user')
@@ -25,9 +26,16 @@ router.post('/register', async (ctx) => {
    */
   // 使用实例化方式,调用10次会实例化10次
   const v = await new RegisterValidator().validate(ctx)
+  //note: 密码加密 盐
+  const salt = bcrypt.genSaltSync(10)
+  /**
+   * 10的意思: 指的是生成盐的成本,越大,花费成本越高,密码安全性越高,一般取默认值
+   * 明文,相同密码加密之后也要不同,防止彩虹攻击
+   */
+  const psw = bcrypt.hashSync(v.get('body.password1'), salt)
   const user = {
     email: v.get('body.email'),
-    password: v.get('body.password1'),
+    password: psw,
     nickname: v.get('body.nickname')
   }
   await User.create(user)
